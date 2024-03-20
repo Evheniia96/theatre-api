5e988a4f5c00fe036df57a7f90aa97c89b4179b3
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-03-20 18:53:24 +0200

    create basic views in the theatre/views.py

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from theatre.models import (
+    Genre,
+    Actor,
+    Play,
+    Perfomance,
+    TheatreHall,
+    Ticket,
+    Reservation,
+)
+from theatre.serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    PlaySerializer,
+    PerfomanceSerializer,
+    TheatreHallSerializer,
+    TicketSerializer,
+    ReservationSerializer,
+)
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class PlayViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewSet):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+
+
+class PerfomanceViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewSet):
+    queryset = Perfomance.objects.all()
+    serializer_class = PerfomanceSerializer
+
+
+class TheatreHallViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, GenreViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+
+
+class TicketViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, GenreViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class ReservationViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, GenreViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
