5d83c28a24efa99792963cfd71965415b6052570
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-03-24 20:17:05 +0200

    update Reservation serializer and view
    -add function create in the theatre/serializers.py
    -add get_queryset and perform_create in the theatre/views.py

--- theatre/views.py
+++ theatre/views.py
@@ -101,3 +101,9 @@ class ReservationViewSet(
 ):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
